fa6e836829744564fc5387ad0a043b1dec9bdf35
leiming <lei_ming@bupt.edu.cn>
2023-10-12 19:17:30 +0800

    update cache policy: cache only if result is not None

--- BlockchainSpider/extensions/cache.py
+++ BlockchainSpider/extensions/cache.py
@@ -9,7 +9,13 @@ class CachePolicy(scrapy.extensions.httpcache.DummyPolicy):
 
     def should_cache_response(self, response, request):
         if super().should_cache_response(response, request):
-            status = json.loads(response.text)['status']
-            if status == '1':
+            data = json.loads(response.text)
+            if isinstance(data.get('result'), list):
                 return True
+        return False
+
+    def is_cached_response_fresh(self, cachedresponse, request):
+        data = json.loads(cachedresponse.text)
+        if isinstance(data.get('result'), list):
+            return True
         return False
